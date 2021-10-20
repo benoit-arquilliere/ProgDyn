bfe15de0903ca5759b5623567c0575c5f52a36b4
benoit-arquilliere <benoit.arquilliere@gmail.com>
2021-10-20 11:30:23 +0200

    Update Knapsack

---
+++ src/Knapsack/knapsack.c
@@ -0,0 +1,18 @@
+/**
+ * @file knapsack.c
+ * @author Benoit ARQUILLIERE
+ * @brief 
+ * @version 0.1
+ * @date 2021-10-20
+ * 
+ * @copyright Copyright (c) 2021
+ * 
+ */
+
+#include "knapsack.h"
+#include <stdio.h>
+
+int sort_Items(){
+    
+    return 0;
+}
---
+++ src/Knapsack/knapsack.h
@@ -0,0 +1,23 @@
+/**
+ * @file knapsack.h
+ * @author Benoit ARQUILLIERE
+ * @brief 
+ * @version 0.1
+ * @date 2021-10-20
+ * 
+ * @copyright Copyright (c) 2021
+ * 
+ */
+
+#ifndef KNAPSACK_H
+#define KNAPSACK_H
+
+typedef struct
+{
+    int quality;
+    int price;
+} Item;
+
+#endif /*KNAPSACK_H*/
+
+int sort_Items();
